df1cc3ef8964bf0d6a9ccafa2f0f0fabea4069a6
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-25 16:43:57 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "HiganWorks LLC"
+maintainer_email "sawanoboriyu@higanworks.com"
+license          "MIT"
+description      "Installs/Configures vargrant_cucmber"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
